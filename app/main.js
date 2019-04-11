161352427637ceef4ffbfac0c2f22e75e4866232
Fabian Große <saphareas@googlemail.com>
2019-04-11 15:53:04 +0200

    First Electron

---
+++ app/main.js
@@ -0,0 +1,28 @@
+const electron = require('electron');
+const {app, BrowserWindow} = electron;
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {nodeIntegration: true}
+  });
+
+  mainWindow.loadFile("app/index.html");
+
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function() {
+  if (process.platform != 'darwin')
+    app.quit();
+});
+
+app.on('activate', function () {
+  if (mainWindow === null) createWindow()
+});
