6cc5ad0cf5d19f73a13bbfb8b75fff7ff70b1c33
Fabian Große <saphareas@googlemail.com>
2019-04-11 14:53:08 +0200

    Working editor

---
+++ editor.js
@@ -0,0 +1,55 @@
+function formatText(callback) {
+  let textarea = document.querySelector("#text > textarea");
+  if (typeof textarea.selectionStart == "number" && typeof textarea.selectionEnd == "number") {
+      let selStart = textarea.selectionStart;
+      let selEnd   = textarea.selectionEnd;
+
+      let selText = textarea.value.slice(selStart, selEnd);
+      let before  = textarea.value.slice(0, selStart);
+      let after   = textarea.value.slice(selEnd);
+
+      let text = before + callback(selText) + after;
+      textarea.value = text;
+  }
+  renderPreview(textarea);
+}
+
+function printBold(selection) {
+  if (selection === "") {
+    return "**bold**"
+  }
+  else if (selection.length > 0) {
+    return "**" + selection + "**"
+  }
+}
+
+function printItalic(selection) {
+  if (selection === "") {
+    return "_italic_"
+  }
+  else if (selection.length > 0) {
+    return "_" + selection + "_"
+  }
+}
+
+function underline(selection) {
+  if (selection === "") {
+    return "<u>underline</u>"
+  }
+  else if (selection.length > 0) {
+    return "<u>" + selection + "</u>"
+  }
+}
+
+function breakLine(selection) {
+  return "<br/>"
+}
+
+function printList(selection) {
+  if (selection === "") {
+    return "- list item"
+  }
+  else if (selection.length > 0) {
+    return "- " + selection + "\n\n"
+  }
+}
--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 
 function navigate(page_name) {
-    let xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        //console.debug(this);
-        if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("content").innerHTML = this.responseText;
-        }
-    };
-    xhttp.open("GET", "pages/"+page_name+".html", true);
-    xhttp.send();
+  let xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    //console.debug(this);
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("content").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "pages/" + page_name + ".html", true);
+  xhttp.send();
 }
 
 function renderPreview(el) {
-    document.getElementById("preview").querySelector("output").innerHTML = marked(el.value);
+  document.getElementById("preview").querySelector("output").innerHTML = marked(el.value);
 }
