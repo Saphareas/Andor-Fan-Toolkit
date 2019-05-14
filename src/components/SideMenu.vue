126d36c86ecb94f441bb3a1ff50482facd05cf7c
Fabian Große <saphareas@googlemail.com>
2019-05-14 10:39:30 +0200

    Moved from electron-vue to Vue CLI Plugin Electron Builder

---
+++ src/components/SideMenu.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav class="w3-sidebar w3-bar-block w3-small w3-hide-small w3-center w3-theme-d4">
+    <img :src="logo">
+    <menu-item v-for="item in items" :key="item.id" :item="item"></menu-item>
+  </nav>
+</template>
+
+<script>
+  import MenuItem from './SideMenu/MenuItem'
+
+  export default {
+    name: 'SideMenu',
+    props: {
+        logo: {
+          type: String,
+          default: "@/assets/logo.png"
+        },
+        items: Array
+    },
+    components: { MenuItem },
+    methods: {}
+  }
+</script>
+
+<style lang="scss" scoped>
+  nav {
+    >img {width: 100%}
+    >menu-item{width: 100%;}
+  }
+</style>
---
+++ src/components/SideMenu/MenuItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <a :href="item.url" class="w3-bar-item w3-button w3-padding-large w3-theme">
+    <i class="fa fa-home w3-xxlarge"></i>
+    <img :src="item.icon">
+    <p>{{item.title}}</p>
+  </a>
+</template>
+
+<script>
+  export default {
+    name: 'MenuItem',
+    props: {
+        item: Object
+    },
+    components: { MenuItem },
+    methods: {}
+  }
+</script>
+
+<style lang="scss" scoped>
+  img {
+    width: 100%;
+    padding: 6px}
+</style>
