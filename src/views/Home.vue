b4b0a32e9e305b136c86ac355b5582da23757d55
Fabian Große <saphareas@googlemail.com>
2019-06-30 13:02:11 +0200

    Delete tab control stuff

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="home">
-    <TabMenu :context="lorem" />
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
@@ -9,18 +8,10 @@
 <script>
   // @ is an alias to /src
   import HelloWorld from '@/components/HelloWorld.vue'
-  import TabMenu from "@/components/TabMenu.vue"
 
   export default {
     name: 'Home',
-    components: {HelloWorld, TabMenu},
-    data: function() {
-      return {
-        lorem: {
-          cards: [{index:"A"},{index:"B"},{index:"C"},{index:"D"},{index:"E"}]
-        }
-      }
-    }
+    components: {HelloWorld}
   }
 </script>
 
--- src/views/StoryEditor.vue
+++ src/views/StoryEditor.vue
@@ -6,21 +6,19 @@ https://github.com/Saphareas/Andor-Fan-Toolkit/blob/master/LICENSE
 
 <template>
   <div>
-    <TabMenu id="tabMenu" :context="storyCardsContext" />
     <StoryEditor style="padding:0 20px" />
   </div>
 </template>
 
 <script>
-  import TabMenu from "@/components/TabMenu.vue";
   import StoryEditor from "@/components/StoryEditor.vue";
 
   export default {
     name: 'StoryEditorView',
-    components: {TabMenu, StoryEditor},
-    data: function() {
-      return {
-        storyCardsContext: this.$root.$data.story
+    components: {StoryEditor},
+    computed: {
+      storyCardsContext: function() {
+        return this.$root.$data.story;
       }
     }
   }
